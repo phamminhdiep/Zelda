e3f288c55eb1f4ce1458a24f1fad5f77bb359f5e
Diep <hyusieunhan@gmail.com>
2022-10-30 23:25:20 +0700

    tao ui thanh mau & mana

---
+++ code/test.py
@@ -0,0 +1,4 @@
+def greeting(name: str) -> str:
+  return 1
+
+print (type((greeting("12"))))
---
+++ code/ui.py
@@ -0,0 +1,25 @@
+import pygame
+from settings import *
+
+class UI:
+    def __init__(self):
+        #general
+        self.display_surface = pygame.display.get_surface()
+        self.font = pygame.font.Font(UI_FONT,UI_FONT_SIZE)
+
+        #bar setup
+        self.health_bar_rect = pygame.Rect(10,10,HEALTH_BAR_WIDTH,BAR_HEIGHT)
+        self.energy_bar_rect = pygame.Rect(10,34,ENERGY_BAR_WIDTH,BAR_HEIGHT)
+
+    def show_bar(self,current,max_amount,bg_rect,color):
+        #draw background
+        pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
+
+        #draw current stats
+        now_rect = bg_rect.copy()
+        now_rect.width =  int(bg_rect.width * (current/max_amount))
+        pygame.draw.rect(self.display_surface,color,now_rect)
+
+    def display(self,player):
+        self.show_bar(player.health,player.stats['health'],self.health_bar_rect,HEALTH_COLOR)
+        self.show_bar(player.energy,player.stats['energy'],self.energy_bar_rect,ENERGY_COLOR)
