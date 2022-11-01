e39125f9cb7271c1b81e151be7045f3e6cac6097
Diep <hyusieunhan@gmail.com>
2022-11-01 21:46:57 +0700

    magic setup

--- code/level.py
+++ code/level.py
@@ -55,10 +55,20 @@ class Level:
                             surface = graphics['object'][int(col)]
                             Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surface)
 
-        self.player = Player((2000, 1400), [self.visible_sprites], self.obstacle_sprites,self.creat_attack,self.destroy_attack)
+        self.player = Player((2000, 1400),
+                             [self.visible_sprites],
+                             self.obstacle_sprites,
+                             self.create_attack,
+                             self.destroy_attack,
+                             self.create_magic)
 
-    def creat_attack(self):
+    def create_attack(self):
         self.current_attack = Weapon(self.player,[self.visible_sprites])
+
+    def create_magic(self,style,strength,cost):
+        print(style)
+        print(strength)
+        print(cost)
 
     def destroy_attack(self):
         if self.current_attack:
--- code/ui.py
+++ code/ui.py
@@ -11,6 +11,20 @@ class UI:
         self.health_bar_rect = pygame.Rect(10,10,HEALTH_BAR_WIDTH,BAR_HEIGHT)
         self.energy_bar_rect = pygame.Rect(10,34,ENERGY_BAR_WIDTH,BAR_HEIGHT)
 
+        #weapon list
+        self.weapon_graphics = []
+        for weapon in weapon_data.values():
+            path = weapon['graphic']
+            weapon_graphic = pygame.image.load(path).convert_alpha()
+            self.weapon_graphics.append(weapon_graphic)
+
+        #magic list
+        self.magic_graphics = []
+        for magic in magic_data.values():
+            path = magic['graphic']
+            magic_graphic = pygame.image.load(path).convert_alpha()
+            self.magic_graphics.append(magic_graphic)
+
     def show_bar(self,current,max_amount,bg_rect,color):
         #draw background
         pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
@@ -19,7 +33,45 @@ class UI:
         now_rect = bg_rect.copy()
         now_rect.width =  int(bg_rect.width * (current/max_amount))
         pygame.draw.rect(self.display_surface,color,now_rect)
+        pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,bg_rect,3)
+
+    def show_exp(self,exp):
+        text_surf = self.font.render(str(int(exp)),False,TEXT_COLOR)
+        x = self.display_surface.get_size()[0]-20
+        y = self.display_surface.get_size()[1]-20
+        text_rect = text_surf.get_rect(bottomright = (x,y))
+
+        pygame.draw.rect(self.display_surface,UI_BG_COLOR,text_rect.inflate(20,20))
+        self.display_surface.blit(text_surf,text_rect)
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, text_rect.inflate(20, 20),3)
+
+    def selection_box(self,left,top,has_switch):
+        bg_rect = pygame.Rect(left,top,ITEM_BOX_SIZE,ITEM_BOX_SIZE)
+        pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
+        if has_switch:
+            pygame.draw.rect(self.display_surface,UI_BORDER_COLOR_ACTIVE, bg_rect,3)
+
+        else: pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, bg_rect,3)
+        return bg_rect
+
+    def weapon_overlay(self,weapon_index,has_switch):
+        bg_rect = self.selection_box(10, 630,has_switch)
+
+        weapon_surf = self.weapon_graphics[weapon_index]
+        weapon_rect = weapon_surf.get_rect(center = bg_rect.center)
+        self.display_surface.blit(weapon_surf,weapon_rect)
+
+    def magic_overlay(self,magic_index,has_switch):
+        bg_rect = self.selection_box(80,635,has_switch)
+
+        magic_surf = self.magic_graphics[magic_index]
+        magic_rect = magic_surf.get_rect(center=bg_rect.center)
+        self.display_surface.blit(magic_surf, magic_rect)
 
     def display(self,player):
         self.show_bar(player.health,player.stats['health'],self.health_bar_rect,HEALTH_COLOR)
         self.show_bar(player.energy,player.stats['energy'],self.energy_bar_rect,ENERGY_COLOR)
+
+        self.show_exp(player.exp)
+        self.weapon_overlay(player.weapon_index,not player.can_switch_weapon)
+        self.magic_overlay(player.magic_index, not player.can_switch_magic)
